1e5319da59c2dddc4971b7606208dd221d8d6d70
Kenny Wu <1079346331@qq.com>
2022-12-19 12:12:26 +0800

    Update train.py

--- train.py
+++ train.py
@@ -37,117 +37,81 @@ class Net(nn.Module):
         return output
 
 
-def main(args):
-    if not os.path.exists(args.train_data_dir):
-        path: Path = Path(args.train_data_dir)
+def main(epochs, learning_rate, device, train_batch_size, val_batch_size, train_data_dir, val_data_dir, api_key,
+         workspace, project_name, use_comet):
+    if not os.path.exists(train_data_dir):
+        path: Path = Path(train_data_dir)
         path.mkdir(exist_ok=False, parents=True)
-    if not os.path.exists(args.val_data_dir):
-        path: Path = Path(args.val_data_dir)
+    if not os.path.exists(val_data_dir):
+        path: Path = Path(val_data_dir)
         path.mkdir(exist_ok=False, parents=True)
 
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
     ])
-    train_data = datasets.MNIST(args.train_data_dir, train=True, download=True, transform=transform)
-    val_data = datasets.MNIST(args.val_data_dir, train=False, download=True, transform=transform)
-    train_loader = DataLoader(train_data, batch_size=args.train_batch_size, shuffle=True)
-    val_loader = DataLoader(val_data, batch_size=args.val_batch_size, shuffle=True)
+    train_data = datasets.MNIST(train_data_dir, train=True, download=True, transform=transform)
+    val_data = datasets.MNIST(val_data_dir, train=False, download=True, transform=transform)
+    train_loader = DataLoader(train_data, batch_size=train_batch_size, shuffle=True)
+    val_loader = DataLoader(val_data, batch_size=val_batch_size, shuffle=True)
 
     model = Net()
-    model = model.to(device=args.device)
+    model = model.to(device=device)
     loss_function = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    if args.use_comet == True:
-
+    if use_comet == True:
         experiment = Experiment(
-            api_key=args.api_key,
-            project_name=args.project_name,
-            workspace=args.workspace
+            api_key=api_key,
+            project_name=project_name,
+            workspace=workspace,
         )
-
         parameters = {
-            "epochs": args.epochs,
-            "learning_rate": args.learning_rate,
-            "device": args.device,
-            "train_batch_size": args.train_batch_size,
-            "val_batch_size": args.val_batch_size
+            "epochs": epochs,
+            "learning_rate": learning_rate,
+            "device": device,
+            "train_batch_size": train_batch_size,
+            "val_batch_size": val_batch_size
         }
         experiment.log_parameters(parameters)
 
-        for epoch in range(1, args.epochs + 1):
-            pbar = tqdm(total=len(train_data), desc='Epoch:{}/{}'.format(epoch, args.epochs))
-            model.train()
-            mean_train_loss = 0
-            step = 0
-            for steps, (images, labels) in enumerate(train_loader, start=1):
-                images = images.to(device=args.device)
-                labels = labels.to(device=args.device)
-                output = model(images)
-                train_loss = loss_function(output, labels)
-                mean_train_loss += train_loss.item()
-                optimizer.zero_grad()
-                train_loss.backward()
-                optimizer.step()
-                step = steps
-                pbar.update(len(images))
-            mean_train_loss = mean_train_loss / step
+    for epoch in range(1, epochs + 1):
+        pbar = tqdm(total=len(train_data), desc='Epoch:{}/{}'.format(epoch, epochs))
+        model.train()
+        mean_train_loss = 0
+        step = 0
+        for steps, (images, labels) in enumerate(train_loader, start=1):
+            images = images.to(device=device)
+            labels = labels.to(device=device)
+            output = model(images)
+            train_loss = loss_function(output, labels)
+            mean_train_loss += train_loss.item()
+            optimizer.zero_grad()
+            train_loss.backward()
+            optimizer.step()
+            step = steps
+            pbar.update(len(images))
+        mean_train_loss = mean_train_loss / step
 
-            model.eval()
-            mean_val_loss = 0
-            step = 0
-            with torch.no_grad():
-                for steps, (images, labels) in enumerate(val_loader, start=1):
-                    images = images.to(device=args.device)
-                    labels = labels.to(device=args.device)
-                    output = model(images)
-                    val_loss = loss_function(output, labels)
-                    mean_val_loss += val_loss.item()
-                    step = steps
-                mean_val_loss = mean_val_loss / step
-                pbar.set_postfix_str('train_loss:{:.6f},val_loss:{:.6f}'.format(mean_train_loss, mean_val_loss))
-            metrics = {'train_loss': mean_train_loss,
-                       'val_loss': mean_val_loss
-                       }
-            experiment.log_metrics(metrics, epoch=epoch)
-            pbar.close()
-    else:
-        for epoch in range(1, args.epochs + 1):
-            pbar = tqdm(total=len(train_data), desc='Epoch:{}/{}'.format(epoch, args.epochs))
-            model.train()
-            mean_train_loss = 0
-            step = 0
-            for steps, (images, labels) in enumerate(train_loader, start=1):
-                images = images.to(device=args.device)
-                labels = labels.to(device=args.device)
+        model.eval()
+        mean_val_loss = 0
+        step = 0
+        with torch.no_grad():
+            for steps, (images, labels) in enumerate(val_loader, start=1):
+                images = images.to(device=device)
+                labels = labels.to(device=device)
                 output = model(images)
-                train_loss = loss_function(output, labels)
-                mean_train_loss += train_loss.item()
-                optimizer.zero_grad()
-                train_loss.backward()
-                optimizer.step()
+                val_loss = loss_function(output, labels)
+                mean_val_loss += val_loss.item()
                 step = steps
-                pbar.update(len(images))
-            mean_train_loss = mean_train_loss / step
-
-            model.eval()
-            mean_val_loss = 0
-            step = 0
-            with torch.no_grad():
-                for steps, (images, labels) in enumerate(val_loader, start=1):
-                    images = images.to(device=args.device)
-                    labels = labels.to(device=args.device)
-                    output = model(images)
-                    val_loss = loss_function(output, labels)
-                    mean_val_loss += val_loss.item()
-                    step = steps
-                mean_val_loss = mean_val_loss / step
-                pbar.set_postfix_str('train_loss:{:.6f},val_loss:{:.6f}'.format(mean_train_loss, mean_val_loss))
-            metrics = {'train_loss': mean_train_loss,
-                       'val_loss': mean_val_loss
-                       }
+            mean_val_loss = mean_val_loss / step
+            pbar.set_postfix_str('train_loss:{:.6f},val_loss:{:.6f}'.format(mean_train_loss, mean_val_loss))
             pbar.close()
+            if use_comet == True:
+                metrics = {'train_loss': mean_train_loss,
+                           'val_loss': mean_val_loss
+                           }
+                experiment.log_metrics(metrics, epoch=epoch)
 
 
 def parse_args():
@@ -159,14 +123,27 @@ def parse_args():
     parser.add_argument('--val_batch_size', type=int, default=3000)
     parser.add_argument('--train_data_dir', type=str, default='./data/train/')
     parser.add_argument('--val_data_dir', type=str, default='./data/val/')
-    parser.add_argument('--api_key', type=str, default='',help='your_comet_account_api_key')
-    parser.add_argument('--workspace', type=str, default='',help='your_workspace')
-    parser.add_argument('--project_name', type=str, default='',help='your_project_name')
+    parser.add_argument('--api_key', type=str, default='', help='your_comet_account_api_key')
+    parser.add_argument('--workspace', type=str, default='', help='your_workspace')
+    parser.add_argument('--project_name', type=str, default='', help='your_project_name')
     parser.add_argument('--use_comet', type=bool, default=False)
     args = parser.parse_args()
-    return args
+    parameters = {
+        'epochs': args.epochs,
+        'learning_rate': args.learning_rate,
+        'device': args.device,
+        'train_batch_size': args.train_batch_size,
+        'val_batch_size': args.val_batch_size,
+        'train_data_dir': args.train_data_dir,
+        'val_data_dir': args.val_data_dir,
+        'api_key': args.api_key,
+        'workspace': args.workspace,
+        'project_name': args.project_name,
+        'use_comet': args.use_comet
+    }
+    return parameters
 
 
 if __name__ == "__main__":
     args = parse_args()
-    main(args)
+    main(**args)
